4fb2822da40f3f4aa22466b2e6effd85369d2aaa
tannghe07 <tannghe07@gmail.com>
2023-07-24 23:14:05 +0700

    create route and controller

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//user route
+Route::resource('user', 'UserController');
+
+//post route
+Route::get('/posts', [\App\Http\Controllers\PostsController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [\App\Http\Controllers\PostsController::class, 'create'])->name('posts.create');
+Route::post('/posts', [\App\Http\Controllers\PostsController::class, 'store'])->name('posts.store');
+Route::get('/posts/{id}', [\App\Http\Controllers\PostsController::class, 'show'])->name('posts.show');
+Route::get('/posts/{id}/edit', [\App\Http\Controllers\PostsController::class, 'edit'])->name('posts.edit');
+Route::put('/posts/{id}', [\App\Http\Controllers\PostsController::class, 'update'])->name('posts.update');
+Route::delete('/posts', [\App\Http\Controllers\PostsController::class, 'destroy'])->name('posts.delete');
